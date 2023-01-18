a4dc78097edcedecc95ce7758d9f4b1d84a97375
Edsogarc <nosdesdhm@gmail.com>
2023-01-18 19:29:16 -0300

    Exercicio Reflection

---
+++ ProjetoReflection/Program.cs
@@ -0,0 +1,43 @@
+﻿using ProjetoReflection;
+using System.Reflection;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DisplayPublicProperties();
+    }
+
+    public static void DisplayPublicProperties()
+    {
+        PropertyInfo[] propertyInfos = CreateInstance().GetType().GetProperties();
+        Console.WriteLine(CreateInstance());
+        foreach (PropertyInfo propertyInfo in propertyInfos)
+        {
+            Console.WriteLine($"Propriedade {propertyInfo.Name}, do tipo '" +
+                $"{propertyInfo.PropertyType.Name}'");
+        }
+    }
+    public static Student CreateInstance()
+    {
+        Student student = Activator.CreateInstance<Student>();
+        PropertyInfo[] propertyInfos = student.GetType().GetProperties();
+        foreach (PropertyInfo propertyInfo in propertyInfos)
+        {
+            if (propertyInfo.Name == "Name")
+            {
+                propertyInfo.SetValue(student, "Edson");
+            }
+            if (propertyInfo.Name == "University")
+            {
+                propertyInfo.SetValue(student, "Univesp");
+            }
+            if (propertyInfo.Name == "RollNumber")
+            {
+                propertyInfo.SetValue(student, 3);
+            }
+        }
+        return student;
+    }
+}
---
+++ ProjetoReflection/Student.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ProjetoReflection
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string University { get; set; }
+        public int RollNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {University} - {RollNumber}";
+        }
+    }
+}
